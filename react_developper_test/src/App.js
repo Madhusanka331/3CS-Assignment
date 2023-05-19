c05f46f4edfdadd2b83143ba19154ff7581f8708
Chathura Madhusanka <86108734+Madhusanka331@users.noreply.github.com>
2023-05-19 22:39:50 +0530

    create routes

--- react_developper_test/src/App.js
+++ react_developper_test/src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
 import {BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import HomePage from "./components/HomePage";
+import UserInformation from "./components/UserInformation";
 function App() {
   return (
     <Router>
+    <Routes>
+    <Route path="/" element={<HomePage />} />
+    <Route path="/view" element={<UserInformation/>} exact />
+    </Routes>
     </Router>
   );
 }
--- react_developper_test/src/components/UserInformation.js
+++ react_developper_test/src/components/UserInformation.js
@@ -3,7 +3,7 @@ import React from 'react'
 const UserInformation = () => {
   return (
     <div>
-      
+      User Information
     </div>
   )
 }
